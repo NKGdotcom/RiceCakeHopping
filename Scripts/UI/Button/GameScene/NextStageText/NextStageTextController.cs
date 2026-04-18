ba93f6b4c9b883cc372207ae441c3bb9153439c0
NKG <naka1028koki@gmail.com>
2026-04-19 00:53:20 +0900

    Add files via upload

--- Scripts/UI/Button/GameScene/NextStageText/NextStageTextController.cs
+++ Scripts/UI/Button/GameScene/NextStageText/NextStageTextController.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// 次のステージに進むボタン
+/// リザルト画面から次のステージに進む
 /// </summary>
 public class NextStageTextController : BaseButton
 {
+    [Header("コンポーネント参照")]
+    [Tooltip("テキストの色を変えるアニメーション")]
     [SerializeField] private TextAnimation textAnimation;
+    [Tooltip("フェードアウト")]
     [SerializeField] private FadeOutController fadeOutController;
+    [Tooltip("シーンの遷移をする")]
     [SerializeField] private TransitionScene transitionScene;
 
     // Start is called before the first frame update
@@ -21,25 +25,44 @@ public class NextStageTextController : BaseButton
         if (fadeOutController == null) { Debug.LogError("fadeOutControllerが参照されていません。"); return; }
         if (transitionScene == null) { Debug.LogError("transitionSceneが参照されていません"); return; }
     }
+
+    /// <summary>
+    /// ボタンの上にマウスを置いたら色を変える
+    /// </summary>
     public override void ButtonEnter()
     {
         textAnimation.RedChangeColor();
     }
 
+    /// <summary>
+    /// ボタンの上からマウスを離したら色を元に戻す
+    /// </summary>
     public override void ButtonExit()
     {
         textAnimation.RedChangeDefaultColor();
     }
 
+    /// <summary>
+    /// ボタンの上でマウスをクリックしたら次のステージに遷移
+    /// </summary>
     public override void ButtonClick()
     {
-        SoundManager.Instance.PlaySE(SESource.button);
+        SoundManager.Instance.PlaySE(SESource.BUTTON);
         NextStage().Forget();
     }
+
+    /// <summary>
+    /// 次のステージに進む
+    /// </summary>
+    /// <returns></returns>
     private async UniTaskVoid NextStage()
     {
         var _token = this.GetCancellationTokenOnDestroy();
+
+        //フェードアウトの処理が終わるまで待機
         await fadeOutController.WaitFadeOutAsync(_token);
+
+        //次のステージに移る
         transitionScene.ToNextStage();
     }
 }
