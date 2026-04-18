d976a1b90b4795acb44f4be09443e1775bc7ee5f
NKG <naka1028koki@gmail.com>
2026-04-19 00:56:46 +0900

    Add files via upload

--- Scripts/UI/Button/Title/HowToPlayText/HowToPlayTextController.cs
+++ Scripts/UI/Button/Title/HowToPlayText/HowToPlayTextController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// タイトルの遊び方のテキストボタン
+/// タイトルの遊び方のページに進むボタン
 /// </summary>
 public class HowToPlayTextController : BaseButton
 {
+    [Header("コンポーネント参照")]
+    [Tooltip("テキストの色を変えるアニメーション")]
     [SerializeField] private TextAnimation textAnimation;
+    [Tooltip("タイトルでのアニメーション")]
     [SerializeField] private TitleAnimation titleAnimation;
+    [Tooltip("遊び方のページ")]
     [SerializeField] private PageController howToPlayPageController;
 
     private void Awake()
@@ -18,26 +22,32 @@ public class HowToPlayTextController : BaseButton
         if (howToPlayPageController == null) { Debug.LogError("howToPlayPageControllerが参照されていません"); return; }
     }
 
+    /// <summary>
+    /// ボタンの上にマウスを置いたら色を変える
+    /// </summary>
     public override void ButtonEnter()
     {
         textAnimation.RedChangeColor();
     }
 
+    /// <summary>
+    /// ボタンの上からマウスを離したら色を元に戻す
+    /// </summary>
     public override void ButtonExit()
     {
         textAnimation.RedChangeDefaultColor();
     }
 
+    /// <summary>
+    /// ボタンの上でマウスをクリックしたら次のステージに遷移
+    /// </summary>
     public override void ButtonClick()
     {
-        SoundManager.Instance.PlaySE(SESource.button);
-        OpenHowToPlayPage();
-    }
+        SoundManager.Instance.PlaySE(SESource.BUTTON);
 
-    //遊び方のページを開く
-    private void OpenHowToPlayPage()
-    {
+        //遊び方のページを開く
         titleAnimation.MovetoHowtoPlayPage();
+
         howToPlayPageController.SetPage();
     }
 }
