eba77eef8a69c71bf3f6f3d5c7a1c0d5f92f128d
NKG <naka1028koki@gmail.com>
2026-04-19 00:51:27 +0900

    Add files via upload

--- Scripts/UI/Button/GameScene/BackToGameText/BackToGameTextController.cs
+++ Scripts/UI/Button/GameScene/BackToGameText/BackToGameTextController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// ゲームに戻るテキストボタン
+/// ポーズ画面からゲーム画面に戻るテキストボタン
 /// </summary>
 public class BackToGameTextController : BaseButton
 {
+    [Header("アニメーション")]
+    [Tooltip("テキストのアニメーション")]
     [SerializeField] private TextAnimation textAnimation;
+    [Tooltip("ポーズボタンのアニメーション")]
     [SerializeField] private PauseAnimation pauseAnimation;
 
     void Awake()
@@ -16,20 +19,33 @@ public class BackToGameTextController : BaseButton
         if (pauseAnimation == null) { Debug.LogError("pauseAnimationが参照されていません。"); return;}
     }
 
+    /// <summary>
+    /// ボタンをマウスの上に置いたら色を変える
+    /// </summary>
     public override void ButtonEnter()
     {
         textAnimation.RedChangeColor();
     }
 
+    /// <summary>
+    /// ボタンをマウスから抜けたら色を変える
+    /// </summary>
     public override void ButtonExit()
     {
         textAnimation.RedChangeDefaultColor();
     }
 
+    /// <summary>
+    /// テキストクリックしたらゲーム画面に戻る
+    /// </summary>
     public override void ButtonClick()
     {
-        SoundManager.Instance.PlaySE(SESource.backButton);
+        SoundManager.Instance.PlaySE(SESource.BACK_BUTTON);
+     
+        //ゲーム実行の状態にする
         StageStateController.Instance.ChangeState(StageState.GAME_PLAY);
+
+        //ポーズ画面を抜け、ゲームを再開
         Time.timeScale = 1;
         pauseAnimation.PauseClose();
     }
