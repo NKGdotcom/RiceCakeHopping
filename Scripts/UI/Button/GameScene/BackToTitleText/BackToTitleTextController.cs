e34c54c102d90077039183c566d48121305cbdea
NKG <naka1028koki@gmail.com>
2026-04-19 00:51:49 +0900

    Add files via upload

--- Scripts/UI/Button/GameScene/BackToTitleText/BackToTitleTextController.cs
+++ Scripts/UI/Button/GameScene/BackToTitleText/BackToTitleTextController.cs
@@ -4,40 +4,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// タイトルに戻るテキストボタン
+/// リザルトやポーズ画面からタイトルに戻るテキストボタン
 /// </summary>
 public class BackToTitleTextController : BaseButton
 {
+    [Header("コンポーネント参照")]
+    [Tooltip("テキストの色を変えるアニメーション")]
     [SerializeField] private TextAnimation textAnimation;
+    [Tooltip("フェードアウトのアニメーション")]
     [SerializeField] private FadeOutController fadeOutController;
+    [Tooltip("シーンの遷移")]
     [SerializeField] private TransitionScene transitionScene;
 
     void Awake()
     {
         if (textAnimation == null) { Debug.LogError("textAnimationが参照されていません。"); return; }
         if (fadeOutController == null) { Debug.LogError("fadeOutControllerが参照されていません。"); return; }
+        if(transitionScene == null) { Debug.LogError("transitionSceneが参照されていません"); return; }
     }
 
+    /// <summary>
+    /// ボタンの上にマウスを置いたら赤色を変える
+    /// </summary>
     public override void ButtonEnter()
     {
         textAnimation.RedChangeColor();
     }
 
+    /// <summary>
+    /// ボタンの上からマウスを離したら色を元に戻す
+    /// </summary>
     public override void ButtonExit()
     {
         textAnimation.RedChangeDefaultColor();
     }
 
+    /// <summary>
+    /// ボタンをクリックし、タイトルに戻る
+    /// </summary>
     public override void ButtonClick()
     {
-        SoundManager.Instance.PlaySE(SESource.button);
+        SoundManager.Instance.PlaySE(SESource.BUTTON);
         BackTitle().Forget();
     }
+
+    /// <summary>
+    /// タイトルに戻るための遷移
+    /// </summary>
+    /// <returns></returns>
     private async UniTaskVoid BackTitle()
     {
+        //ポーズ画面から呼ばれた場合を考慮し、時間の進みを元に戻す
         Time.timeScale = 1.0f;
+
         var _token = this.GetCancellationTokenOnDestroy();
+
+        //フェードアウト完了を待機
         await fadeOutController.WaitFadeOutAsync(_token);
+
+        //タイトルへ遷移
         transitionScene.ToTitle();
     }
 }
